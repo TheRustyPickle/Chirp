7c9219901be6c2fd9bf8f0564c9f72e0e04b1b0a
Rusty Pickle <mohammad.sakib0194@gmail.com>
2023-08-31 04:15:06 +0600

    Basic ws server on actix

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,48 @@
-fn main() {
-    println!("It is working")
+use actix::{Actor, AsyncContext, StreamHandler};
+use actix_web::{web, App, Error, HttpRequest, HttpResponse, HttpServer};
+use actix_web_actors::ws;
+use std::time::Duration;
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new().route("/ws/", web::get().to(index)))
+        .bind(("127.0.0.1", 8080))?
+        .run()
+        .await
+}
+
+struct MyWs;
+
+impl Actor for MyWs {
+    type Context = ws::WebsocketContext<Self>;
+
+    fn started(&mut self, ctx: &mut Self::Context) {
+        ctx.run_interval(Duration::from_secs(1), move |_, n| {
+            n.text(format!(
+                "random reply with number {}",
+                rand::random::<u64>()
+            ))
+        });
+    }
+}
+
+/// Handler for ws::Message message
+impl StreamHandler<Result<ws::Message, ws::ProtocolError>> for MyWs {
+    fn handle(&mut self, msg: Result<ws::Message, ws::ProtocolError>, ctx: &mut Self::Context) {
+        match msg {
+            Ok(ws::Message::Ping(msg)) => ctx.pong(&msg),
+            Ok(ws::Message::Text(text)) => {
+                println!("Received message: {}", text);
+                ctx.text(format!("You said: {}", text));
+            }
+            Ok(ws::Message::Binary(bin)) => ctx.binary(bin),
+            _ => (),
+        }
+    }
+}
+
+async fn index(req: HttpRequest, stream: web::Payload) -> Result<HttpResponse, Error> {
+    let resp = ws::start(MyWs {}, &req, stream);
+    println!("{:?}", resp);
+    resp
 }
