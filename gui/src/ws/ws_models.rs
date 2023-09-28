449567c8efa935804acccd545f2308d937f94ac0
Rusty Pickle <mohammad.sakib0194@gmail.com>
2023-09-28 06:09:37 +0600

    Removed sending unnecessary data

--- gui/src/ws/ws_models.rs
+++ gui/src/ws/ws_models.rs
@@ -3,8 +3,27 @@ use serde::{Deserialize, Serialize};
 
 use crate::user::UserObject;
 
-/// Used for sending or relevant data to create an UserObject
-/// An optional message field to pass messages along with the user data
+/// Types of request that are processed by the GUI to WS currently
+#[derive(Debug, Clone)]
+pub enum RequestType {
+    // Ask the WS to create a new user
+    CreateNewUser,
+    // Broadcast name update to the WS
+    NameUpdated(String),
+    // Broadcast image update to the WS
+    ImageUpdated(String),
+    // Try to reconnect with the WS again
+    ReconnectUser,
+    // Send my IDs to the WS
+    UpdateIDs,
+    // Send a message to another user
+    SendMessage(String),
+    // Ask the WS for a specific user info
+    GetUserData(u64),
+}
+
+/// Used for sending or receiving relevant data to create an UserObject
+/// An optional message field to receive messages along with the user data
 #[derive(Serialize, Deserialize)]
 pub struct FullUserData {
     pub user_id: u64,
@@ -33,13 +52,14 @@ impl FullUserData {
         };
         serde_json::to_string(&user_data).unwrap()
     }
+
+    pub fn from_json(data: &str) -> Self {
+        serde_json::from_str(data).unwrap()
+    }
 }
 
 #[derive(Serialize, Deserialize)]
 pub struct UserIDs {
-    // When receiving from ws owner_id won't be included so skip it
-    #[serde(skip_deserializing)]
-    pub owner_id: u64,
     pub user_id: u64,
     pub user_token: String,
 }
@@ -47,7 +67,6 @@ pub struct UserIDs {
 impl UserIDs {
     pub fn new_json(user_object: &UserObject) -> String {
         let id_data = UserIDs {
-            owner_id: user_object.owner_id(),
             user_id: user_object.user_id(),
             user_token: user_object.user_token(),
         };
@@ -59,31 +78,17 @@ impl UserIDs {
     }
 }
 
-/// Types of request that are processed by the GUI to WS currently
-#[derive(Debug, Clone)]
-pub enum RequestType {
-    CreateNewUser,
-    NameUpdated(String),
-    ImageUpdated(String),
-    ReconnectUser,
-    UpdateIDs,
-    SendMessage(String),
-    GetUserData(u64),
-}
-
 #[derive(Debug, Serialize, Clone)]
 pub struct MessageData {
-    pub from_user: u64,
     pub to_user: u64,
     pub message: String,
     pub user_token: String,
 }
 
 impl MessageData {
-    pub fn new_json(from_user: u64, to_user: u64, message: String, user_token: String) -> String {
+    pub fn new_json(to_user: u64, message: String, user_token: String) -> String {
         let data = MessageData {
             message,
-            from_user,
             to_user,
             user_token,
         };
