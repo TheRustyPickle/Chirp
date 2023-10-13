eb4fe31950fe549aeb4e39af1a00116203602495
Rusty Pickle <mohammad.sakib0194@gmail.com>
2023-10-14 04:56:54 +0600

    Comment

--- gui/src/window.rs
+++ gui/src/window.rs
@@ -116,6 +116,7 @@ impl Window {
         let imp = self.imp();
         imp.stack.set_visible_child_name("main");
 
+        // If a new user in the user row is selected, this is activated
         imp.user_list
             .connect_row_activated(clone!(@weak self as window => move |listbox, row| {
                 let last_index = window.imp().last_selected_user.get();
@@ -140,6 +141,7 @@ impl Window {
                 window.bind();
             }));
 
+            // The event on New Chat button clicked 
         self.imp()
             .new_chat
             .connect_clicked(clone!(@weak self as window => move |_| {
@@ -147,12 +149,14 @@ impl Window {
                 prompt.present();
             }));
 
+            // The event on Profile button clicked
         self.imp()
             .my_profile
             .connect_clicked(clone!(@weak self as window => move |_| {
                 UserProfile::new(window.get_chatting_from(), &window);
             }));
 
+            // The event on the send button beside the textview
         self.imp()
             .send_button
             .connect_clicked(clone!(@weak self as window => move |_| {
@@ -160,6 +164,7 @@ impl Window {
                 window.grab_focus();
             }));
 
+        // If the scrollbar size is changed, scroll to the bottom
         let scroller_bar = self.imp().message_scroller.get();
         let vadjust = scroller_bar.vadjustment();
         vadjust.connect_changed(clone!(@weak vadjust => move |adjust| {
@@ -167,6 +172,8 @@ impl Window {
             vadjust.set_value(upper);
         }));
 
+        // If the message typing space is empty, show the background text + disable the send button
+        // else remove the text and enable the send button
         self.imp().message_entry.get().buffer().connect_changed(
             clone!(@weak self as window => move |buffer| {
                 let char_count = buffer.char_count();
@@ -188,6 +195,7 @@ impl Window {
     }
 
     fn setup_actions(&self) {
+        // The action that is emitted on CTRL + Enter key. This is used for sending messages
         let send_message_action = SimpleAction::new("send-message", None);
         send_message_action.connect_activate(clone!(@weak self as window => move |_, _| {
             window.send_message();
@@ -213,6 +221,7 @@ impl Window {
             .unwrap();
     }
 
+    /// Save owner ID and Token in the predefined location in a json file
     fn save_user_data(&self) {
         let saving_location = self.settings().string("location");
         info!("Saving new user id info on {}", saving_location);
@@ -223,6 +232,7 @@ impl Window {
         file.write_all(id_data.as_bytes()).unwrap();
     }
 
+    /// Check if any existing owner data is available
     fn check_user_data(&self) -> Option<UserIDs> {
         let saving_location = self.settings().string("location");
         if !saving_location.is_empty() {
@@ -240,6 +250,7 @@ impl Window {
         None
     }
 
+    /// Bind the main window header bar's title to the selected chat
     fn bind(&self) {
         let mut bindings = self.imp().bindings.borrow_mut();
         let chatting_with = self.get_chatting_with();
@@ -251,12 +262,14 @@ impl Window {
         bindings.push(title_binding);
     }
 
+    /// Disconnect the last header bar title binding
     fn remove_last_binding(&self) {
         if let Some(binding) = self.imp().bindings.borrow_mut().pop() {
             binding.unbind();
         }
     }
 
+    /// Setup owner profile and bind User model
     fn setup_users(&self) {
         let users = ListStore::new::<UserObject>();
         self.imp().users.set(users).expect("Could not set users");
@@ -265,6 +278,7 @@ impl Window {
         let user_store = self.get_users_liststore();
         let user_list = self.get_user_list();
 
+        // Bind the model so if any user is added to user_store, start creating the new user row
         user_list.bind_model(
             Some(user_store),
             clone!(@weak self as window => @default-panic, move |obj| {
@@ -283,9 +297,9 @@ impl Window {
 
         // Select the first row we just added
         self.get_user_list().row_at_index(0).unwrap().activate();
-        self.bind();
     }
 
+    /// Get the UserObject that is currently selected/chatting with
     pub fn get_chatting_with(&self) -> UserObject {
         self.imp()
             .chatting_with
@@ -294,9 +308,13 @@ impl Window {
             .expect("Expected an UserObject")
     }
 
+    /// Set chatting with the given user
     fn set_chatting_with(&self, user: UserObject) {
         info!("Setting chatting with {}", user.name());
         user.add_queue_to_first(RequestType::NewUserSelection(user.clone()));
+
+        // Bind the model to a specific ListStore so if any new message gets added there
+        // new message row creation also starts
         let message_list = user.messages();
         self.imp().message_list.bind_model(
             Some(&message_list),
@@ -310,6 +328,7 @@ impl Window {
         self.imp().chatting_with.replace(Some(user));
     }
 
+    /// Get the UserObject of the owner/chatting from
     pub fn get_chatting_from(&self) -> UserObject {
         self.imp()
             .own_profile
@@ -318,18 +337,22 @@ impl Window {
             .expect("Expected an UserObject")
     }
 
+    /// Get the owner user id
     pub fn get_owner_id(&self) -> u64 {
         self.get_chatting_from().user_id()
     }
 
+    /// Get the ListStore of where all UserObject is saved
     fn get_users_liststore(&self) -> &ListStore {
         self.imp().users.get().expect("User liststore is not set")
     }
 
+    /// Get the ListStore of a user where all MessageObject is saved
     fn chatting_with_messages(&self) -> ListStore {
         self.get_chatting_with().messages()
     }
 
+    /// Send the text on the Textview as a message 
     fn send_message(&self) {
         let buffer = self.imp().message_entry.buffer();
         let content = buffer
@@ -376,6 +399,7 @@ impl Window {
     pub fn receive_message(&self, message_data: MessageData, other_user: UserObject) {
         let current_message_number = other_user.message_number();
         if current_message_number < message_data.message_number {
+            // Less than current number means it's an old message
             other_user.set_message_number(other_user.message_number() + 1);
         }
 
@@ -402,6 +426,7 @@ impl Window {
             Some(message_data.message_number),
         );
 
+        // Sync messages are gotten in reverse order so they are pushed to the start
         if current_message_number > message_data.message_number {
             other_user.messages().insert(0, &message);
         } else {
@@ -409,6 +434,7 @@ impl Window {
         }
     }
 
+    /// Create a row using a MessageObject for the ListBox
     fn get_message_row(&self, data: &MessageObject) -> ListBoxRow {
         let message_row = MessageRow::new(data.clone(), self);
         let list_row = ListBoxRow::builder()
@@ -423,6 +449,7 @@ impl Window {
         list_row
     }
 
+    /// Create a row using a UserObject for the ListBox
     fn get_user_row(&self, data: &UserObject, css_name: &str) -> ListBoxRow {
         let user_row = UserRow::new(data.clone());
         user_row.imp().user_avatar.add_css_class(css_name);
@@ -434,10 +461,12 @@ impl Window {
             .build()
     }
 
+    /// Used during the startup of the app. Called only once to create the owner profile
     fn create_owner(&self, name: &str) -> UserObject {
         // It's a new user + owner so the ID will be generated on the server side
         let id_data = self.check_user_data();
 
+        // attempt to find previously saved owner data
         let user_data = if let Some(data) = id_data {
             info!("Saved user data found");
             UserObject::new(name, None, None, Some(data.user_id), Some(data.user_token))
@@ -452,7 +481,7 @@ impl Window {
     }
 
     /// Function that handles some WS requests. Every added UserObject will have an instance of this to
-    /// process their requests. user variable calls the function
+    /// process their requests. 
     pub fn handle_ws_message(&self, user: &UserObject, receiver: Receiver<String>) {
         receiver.attach(None, clone!(@weak user as user_object, @weak self as window => @default-return ControlFlow::Break, move |response| {
             let response_data: Vec<&str> = response.splitn(2, ' ').collect();
@@ -486,7 +515,7 @@ impl Window {
         }));
     }
 
-    /// Used to create all UserObject for the self's users ListStore except the owner Object.
+    /// Used to create all UserObject for the self's users ListStore except for the owner UserObject.
     /// Called when New Chat button is used or a message is received but the user was not added
     fn create_user(&self, user_data: FullUserData) {
         info!(
@@ -519,14 +548,17 @@ impl Window {
         self.get_users_liststore().append(&new_user_data);
     }
 
+    /// Get the users ListBox
     fn get_user_list(&self) -> ListBox {
         self.imp().user_list.get()
     }
 
+    /// Get the color is that is being used for owner UserObject
     fn get_owner_name_color(&self) -> String {
         self.get_chatting_from().name_color()
     }
 
+    /// Start focus on the textview
     fn grab_focus(&self) {
         self.imp().message_entry.grab_focus();
     }
@@ -547,6 +579,7 @@ impl Window {
         }
     }
 
+    /// Iters through every UserObject and tries to reconnect to the WebSocket server
     pub fn reload_user_ws(&self) {
         let user_list = self.get_users_liststore();
 
@@ -556,6 +589,7 @@ impl Window {
         }
     }
 
+    /// Find a UserObject based on the User ID 
     pub fn find_user(&self, target_id: u64) -> Option<UserObject> {
         let user_list = self.get_users_liststore();
         for user_data in user_list.iter() {
@@ -567,6 +601,7 @@ impl Window {
         None
     }
 
+    /// Find and delete a UserObject based on the User ID
     pub fn delete_user(&self, target_id: u64) {
         let user_list = self.get_users_liststore();
         for (index, user_data) in user_list.iter().enumerate() {
