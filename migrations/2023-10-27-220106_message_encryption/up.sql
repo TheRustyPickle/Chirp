123af14f1eebb477e65c53dd6625cf7a75bde588
Rusty Pickle <mohammad.sakib0194@gmail.com>
2023-10-29 04:16:51 +0600

    Update migration

--- migrations/2023-10-27-220106_message_encryption/up.sql
+++ migrations/2023-10-27-220106_message_encryption/up.sql
@@ -1,8 +1,10 @@
 -- Your SQL goes here
 ALTER TABLE messages DROP COLUMN message_text,
-    ADD COLUMN sender_message TEXT,
-    ADD COLUMN receiver_message TEXT,
-    ADD COLUMN sender_key TEXT,
-    ADD COLUMN receiver_key TEXT;
+    ADD COLUMN sender_message BYTEA,
+    ADD COLUMN receiver_message BYTEA,
+    ADD COLUMN sender_key BYTEA,
+    ADD COLUMN receiver_key BYTEA,
+    ADD COLUMN sender_nonce BYTEA,
+    ADD COLUMN receiver_nonce BYTEA;
 ALTER TABLE users
 ADD COLUMN rsa_public_key TEXT NOT NULL;
